0babcbda9e6c2994844730dbf3ee031c5a26caeb
lucashersche <lucas.hersche@stud.kftg.ch>
2019-10-02 09:21:23 +0200

    gdgfsd

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,24 +1,17 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
+var address = {
+    'longitude': 45435234,
+    'latitude': 345934859,
+    'street': 'thundorferstrasse',
+    'zip': 8512,
+    'city': 'St.gallen',
+    'type': 'privat'
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
+        console.log('app.initialize()');
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -27,20 +20,26 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+        console.log('onDeviceReady()');
         this.receivedEvent('deviceready');
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        //var parentElement = document.getElementById(id);
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    helloworld: function(data){
+        console.log('helloworld() Hello ' + data);
+        console.log(address);
     }
 };
 
 app.initialize();
+app.helloworld('World');
